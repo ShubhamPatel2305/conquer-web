6455d32ddd04498c70fd7cfe3cd6fcad60cb5ec2
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-09-27 09:04:37 +0530

    body parser

--- Practice/express/http server/index.js
+++ Practice/express/http server/index.js
@@ -1,8 +1,16 @@
 const express= require('express');
 const app=express();
 const port=3000;
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.json());
+//express gives no functionality to read a request body so we have to use body parser as a middleware to achieve it.
 
 app.get("/", (req,res)=>{
+
+    let data=req.body;
+    console.log(data);
+
     res.send("hello world");
 });
 
