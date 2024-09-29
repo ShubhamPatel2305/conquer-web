973cf3608e5e435f6f6af68157b5ff3d0f432b6d
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-09-29 23:24:43 +0530

    Understanding how key is important while passing a list as a prop to other component because without key react can't identify which item in list was what and will mess up when list gets sorted or changed by any ways

--- Practice/04_react/practice/week-6/basics/src/App.jsx
+++ Practice/04_react/practice/week-6/basics/src/App.jsx
@@ -3,14 +3,44 @@ import { useState } from 'react'
 import './App.css'
 import Test from './components/test'
 import ReduceReRenderUsingMemo from './components/ReduceReRenderUsingMemo'
+import Todo from './components/Todo'
+
+var globalCounter=4;
 
 function App() {
 
+  const [todos, settodos]=useState([{
+    id:1,
+    description:"Learn React",
+  },
+  {
+    id:2,
+    description:"Learn Node",
+  },
+  {
+    id:3,
+    description:"Learn Mongo",
+  } 
+  ]);
+
+  const handleClick = ()=>{
+    // add an element : "new task" to todos array on each click
+    settodos([...todos,{
+      id:globalCounter++,
+      description:"new task"
+    }]);
+  }
   return (
     <>
       <h1>React Basics</h1>
       <Test />
       <ReduceReRenderUsingMemo />
+      {/* send all todos as props to Todo component one by one as the todo accepys only description of todo it has to render */}
+      {todos.map((todo)=>(
+        // eslint-disable-next-line react/jsx-key
+        <Todo  key={todo.id} description={todo.description} />
+      ))}
+      <button onClick={handleClick} style={{border:"1px solid black",width:"200px",height:"50px"}}>Add a new todo</button>
     </>
   )
 }
---
+++ Practice/04_react/practice/week-6/basics/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Todo = ({description}) => {
+  return (
+    <div>{description}</div>
+  )
+}
+
+export default Todo
