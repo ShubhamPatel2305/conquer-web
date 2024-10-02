0dc5016144e700770965c399afef829cdb409c7a
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-03 01:19:15 +0530

    basic atom initialized and got rid of all state variables in app.jsx

--- Practice/04_react/practice/week-7/recoil/src/App.jsx
+++ Practice/04_react/practice/week-7/recoil/src/App.jsx
@@ -6,41 +6,36 @@ import { Navigate } from "react-router-dom";
 
 
 function App() {
-  const [count, setCount] = useState(0);  
-  // wrap anyone that wants to use the teleported value inside a provider
-  // recoil, redux, Themes in mUI
   return (
     <div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
+      <CountContext.Provider>
+        <Count />
       </CountContext.Provider>
     </div>
   )
 }
 
-function Count({setCount}) {
+function Count() {
   return <div>
     <CountRenderer />
-    <Buttons setCount={setCount} />
+    <Buttons />
   </div>
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
   return <div>
-    {count}
+
   </div>
 }
 
-function Buttons({setCount}) {
-  const count = useContext(CountContext);
+function Buttons() {
   return <div>
     <button onClick={() => {
-      setCount(count + 1)
+
     }}>Increase</button>
 
     <button onClick={() => {
-      setCount(count - 1)
+      
     }}>Decrease</button>
   </div>
 }
---
+++ Practice/04_react/practice/week-7/recoil/src/store/atoms/count.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { atom } from "recoil"
+
+export const countAtom= atom({
+    key: 'count',
+    default: 0
+})
