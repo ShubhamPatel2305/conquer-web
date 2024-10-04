1ffc9ddc5dc6dc9cec2ba7c155127e76f6abbf20
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-04 09:31:45 +0530

    atom family , read comments

--- Practice/04_react/practice/week-7/7.4 assignments/7.4/atom-family/src/App.jsx
+++ Practice/04_react/practice/week-7/7.4 assignments/7.4/atom-family/src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   return <RecoilRoot>
     <Todo id={1}/>
     <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    {/* on multiple calls a new atom will not be generated it will be generated only once and then same will be returned each time. same stand for when you update an atom with given id all changes at once  */}
   </RecoilRoot>
 }
 
--- Practice/04_react/practice/week-7/7.4 assignments/7.4/atom-family/src/atoms.js
+++ Practice/04_react/practice/week-7/7.4 assignments/7.4/atom-family/src/atoms.js
@@ -1,6 +1,7 @@
 import { atomFamily } from "recoil";
 import { TODOS } from "./todos";
 
+//Returns a function that returns a writeable RecoilState atom 
 export const todosAtomFamily = atomFamily({
   key: 'todosAtomFamily',
   default: id => {
