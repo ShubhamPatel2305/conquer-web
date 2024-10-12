764c44c9c9404cdcf4d0c5d2f76ef37c1e2d4024
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-12 22:49:27 +0530

    this is where client sideand server side components come in client side components are the ones that use state management in them or even handlers like onclick onhover etc because nextjs while serverside rendering component having onclick says ok there is nothing like oncliclk n server itshandled on client side so these components get rendered on client side but they come with a downside that it removes SEO optiixation also in the component tree try to push down client side rendered components as low as possible because all childs ofa cliend side component also become a client side component

--- Practice/next js/week-14/app/auth/signin/page.tsx
+++ Practice/next js/week-14/app/auth/signin/page.tsx
@@ -1,10 +1,15 @@
 import { TestComponent } from "@/components/Test"
 
 const page = () => {
+
+  function handleClick(){
+    alert("click");
+  }
   return (
     <div>
       sgnin
       <TestComponent />  
+      <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
