2d8729208e9816e3313d12e46c5fd31557ccb387
ShubhamPatel2305 <shubhamcp23@gmail.com>
2024-10-13 23:48:14 +0530

    this is how we attach api calls to backend calls and also how post request are handled by nextjs backend

--- Practice/next js/week-14/app/api/user/route.ts
+++ Practice/next js/week-14/app/api/user/route.ts
@@ -1,8 +1,19 @@
+import { NextRequest } from "next/server";
+
 export function GET(){
     return Response.json({
         email:"shubham@ail.com",
         name:"shubham"
     })
+}
+
+export async function POST(req:NextRequest){
+    const input= await req.json();
+    console.log(input.username);
+    return Response.json({
+        uname:input.username,
+        pass:input.password
+    });
 }
 
 /*
